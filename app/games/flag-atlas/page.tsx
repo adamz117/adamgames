fb6119c94881b76d9f61ee39670712e9756164e4
Adam Zammit <adamzammit117@gmail.com>
2026-09-04 17:11:36 +0200

    Migrate Flag Atlas in as an embedded game page

---
+++ app/games/flag-atlas/page.tsx
@@ -0,0 +1,14 @@
+export default function FlagAtlasPage() {
+  return (
+    <iframe
+      src="/games/flag-atlas.html"
+      style={{
+        display: 'block',
+        width: '100%',
+        height: 'calc(100vh - 65px)',
+        border: 'none',
+      }}
+      title="Flag Atlas"
+    />
+  );
+}
